c1d6a498a0e0e4fcaa7c1500fe854883bde54e25
pablo.aytes <pablo.aytes@auleseebe.upc.edu>
2025-02-11 09:17:51 +0100

    prueba

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,33 @@
 #Codigo para problema 1
 
-plot(1:5)
+mis_dades <- mtcars
+mis_dades
+
+mis_dades$qsec
+
+#promedio
+mean(mis_dades$qsec)
+
+#mediana
+mis_dades$drat
+sort(mis_dades$drat)
+median(mis_dades$drat)
+
+#quartiles
+quantile(mis_dades$drat, 0.25)
+quantile(mis_dades$drat, 0.75)
+IQR(mis_dades$drat)
+
+boxplot(mis_dades$drat)
+quantile(mis_dades$drat, 0.18)
+quantile(mis_dades$mpg, 0.18)
+
+IQR(mis_dades$cyl)
+
+#desviació típica
+sd(mis_dades$cyl)
+
+#variància
+var(mis_dades$qsec)
+x <- mis_dades$qsec
+sum((x-mean(x))^2)/(length(x)-1)
